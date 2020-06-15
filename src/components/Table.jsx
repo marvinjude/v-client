30d2ab6158a1ace2b6e52961be60933539e8aa1e
Jude Agboola <marvinjudehk@gmail.com>
2020-06-15 22:24:05 +0100

    add missing header to table

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -26,6 +26,9 @@ function Table({ data }) {
                 car_model_year
               </th>
               <th className="px-6 py-3 border-b border-gray-200 bg-gray-100 text-left text-xs leading-4 font-medium text-gray-600 uppercase tracking-wider">
+                color 
+              </th>
+              <th className="px-6 py-3 border-b border-gray-200 bg-gray-100 text-left text-xs leading-4 font-medium text-gray-600 uppercase tracking-wider">
                 gender
               </th>
               <th className="px-6 py-3 border-b border-gray-200 bg-gray-100 text-left text-xs leading-4 font-medium text-gray-600 uppercase tracking-wider">
