7e2f39ff8681e4df96a932b91a7230df8abf2c7f
Jude Agboola <marvinjudehk@gmail.com>
2020-06-15 23:03:34 +0100

    remove #(id) head from table

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -8,9 +8,6 @@ function Table({ data }) {
           <thead>
             <tr className="w-full">
               <th className="px-6 py-3 border-b border-gray-200 bg-gray-100 text-left text-xs leading-4 font-medium text-gray-600 uppercase tracking-wider">
-                #
-              </th>
-              <th className="px-6 py-3 border-b border-gray-200 bg-gray-100 text-left text-xs leading-4 font-medium text-gray-600 uppercase tracking-wider">
                 first_name
               </th>
               <th className="px-6 py-3 border-b border-gray-200 bg-gray-100 text-left text-xs leading-4 font-medium text-gray-600 uppercase tracking-wider">
@@ -24,6 +21,9 @@ function Table({ data }) {
               </th>
               <th className="px-6 py-3 border-b border-gray-200 bg-gray-100 text-left text-xs leading-4 font-medium text-gray-600 uppercase tracking-wider">
                 car_model_year
+              </th>
+              <th className="px-6 py-3 border-b border-gray-200 bg-gray-100 text-left text-xs leading-4 font-medium text-gray-600 uppercase tracking-wider">
+                color
               </th>
               <th className="px-6 py-3 border-b border-gray-200 bg-gray-100 text-left text-xs leading-4 font-medium text-gray-600 uppercase tracking-wider">
                 gender
